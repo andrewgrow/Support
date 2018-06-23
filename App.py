6d6ecaaa3fb065a64f8cea0ee52869aeaee93822
Andrew Gahov <andrew.gahov@gmail.com>
2018-06-23 20:32:20 +0300

    Update gitignore

--- App.py
+++ App.py
@@ -1,25 +1,25 @@
 import os
-from os import listdir
-from os.path import isfile, join
 
 # for start open terminal and print
 # py -3 App.py
 
 
-
-
 from os import walk
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+input_path = dir_path + '\input'
+output_path = dir_path + '\output'
 
 files_list = []
-for (dirpath, dirnames, filenames) in walk('./incoming'):
+for (dirpath, dirnames, filenames) in walk(input_path):
     files_list.extend(filenames)
     break
 
+
 if files_list:
-    print("Success")
-    for file in files_list:
-        print(file)
+    print("Incoming path = ", input_path)
+    print(files_list)
+
+
 else:
     print("Wrong")
